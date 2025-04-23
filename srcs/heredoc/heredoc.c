9ded26d0f6bc8cff679c6aadf8bd67a0ab1fe09e
João Miguel Martinho Andrade <joaomart@c2r1s3.42porto.com>
2025-04-23 18:40:15 +0100

    fix norm error on ft_printf_fd.c

--- srcs/heredoc/heredoc.c
+++ srcs/heredoc/heredoc.c
@@ -1,35 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf_fd.c                                     :+:      :+:    :+:   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joaomart <joaomart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/07 13:53:38 by joaomart          #+#    #+#             */
-/*   Updated: 2025/04/23 18:39:20 by joaomart         ###   ########.fr       */
+/*   Created: 2025/04/23 17:39:03 by joaomart          #+#    #+#             */
+/*   Updated: 2025/04/23 17:39:12 by joaomart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/ft_printf_fd.h"
+#include "../../inc/minishell.h"
 
-int	ft_printf_fd(int fd, const char *format, ...)
-{
-	va_list	args;
-	int		count;
-
-	va_start(args, format);
-	count = 0;
-	while (*format)
-	{
-		if (*format == '%')
-			count += ft_print_format_fd(fd, *(++format), args);
-		else
-		{
-			write(fd, format, 1);
-			count++;
-		}
-		format++;
-	}
-	va_end(args);
-	return (count);
-}

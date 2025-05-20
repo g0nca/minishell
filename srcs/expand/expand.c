267d901951940cc498b7aa28560d0708abf9fb9a
Gonçalo Gomes Valente <ggomes-v@c1r15s5.42porto.com>
2025-05-20 15:32:26 +0100

    desisto vou lanchar

--- srcs/expand/expand.c
+++ srcs/expand/expand.c
@@ -6,7 +6,7 @@
 /*   By: ggomes-v <ggomes-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 10:25:27 by ggomes-v          #+#    #+#             */
-/*   Updated: 2025/05/20 14:18:56 by ggomes-v         ###   ########.fr       */
+/*   Updated: 2025/05/20 15:32:05 by ggomes-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,7 +117,6 @@ char *expand_variables(const char *input, char **envp, t_token *list)
     size_t  size;
 
     size = calculate_final_size(input, envp, list);
-    printf("SIZE:%zu\n", size);
     result = (char *)malloc(size + 1);
     if (!result)
         return (NULL);

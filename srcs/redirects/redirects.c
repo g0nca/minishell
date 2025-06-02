a63c838bb4861f611bdc7c2821005bbf7f8ae3f7
João Miguel Martinho Andrade <joaomart@c2r2s1.42porto.com>
2025-06-02 16:32:54 +0100

    fix redirects

--- srcs/redirects/redirects.c
+++ srcs/redirects/redirects.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirects.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: andrade <andrade@student.42.fr>            +#+  +:+       +#+        */
+/*   By: joaomart <joaomart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 16:55:23 by andrade           #+#    #+#             */
-/*   Updated: 2025/05/30 11:16:45 by andrade          ###   ########.fr       */
+/*   Updated: 2025/06/02 15:58:16 by joaomart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,21 @@ int	ft_token_redir_in(t_token *current)
 		fprintf(stderr, "minishell: syntax error near unexpected token `newline'\n");
 		return (1);
 	}
-	
+
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 	{
 		perror("minishell");
 		return (1);
 	}
-	
+
 	if (dup2(fd, STDIN_FILENO) < 0)
 	{
 		perror("minishell: dup2");
 		close(fd);
 		return (1);
 	}
-	
+
 	close(fd);
 	return (0);
 }
@@ -57,21 +57,21 @@ int	ft_token_redir_out(t_token *current)
 		fprintf(stderr, "minishell: syntax error near unexpected token `newline'\n");
 		return (1);
 	}
-	
+
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd < 0)
 	{
 		perror("minishell");
 		return (1);
 	}
-	
+
 	if (dup2(fd, STDOUT_FILENO) < 0)
 	{
 		perror("minishell: dup2");
 		close(fd);
 		return (1);
 	}
-	
+
 	close(fd);
 	return (0);
 }
@@ -89,21 +89,21 @@ int	ft_token_append(t_token *current)
 		fprintf(stderr, "minishell: syntax error near unexpected token `newline'\n");
 		return (1);
 	}
-	
+
 	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
 	if (fd < 0)
 	{
 		perror("minishell");
 		return (1);
 	}
-	
+
 	if (dup2(fd, STDOUT_FILENO) < 0)
 	{
 		perror("minishell: dup2");
 		close(fd);
 		return (1);
 	}
-	
+
 	close(fd);
 	return (0);
 }
@@ -115,7 +115,7 @@ int	setup_redirections(t_token *token)
 
 	current = token;
 	result = 0;
-	
+
 	while (current && result == 0)
 	{
 		if (current->type == TOKEN_REDIR_IN)
@@ -124,10 +124,10 @@ int	setup_redirections(t_token *token)
 			result = ft_token_redir_out(current);
 		else if (current->type == TOKEN_APPEND)
 			result = ft_token_append(current);
-		
+
 		current = current->next;
 	}
-	
+
 	return (result);
 }
 

2dc3bbba28220cec326078edb99339285a446529
João Miguel Martinho Andrade <joaomart@c2r3s1.42porto.com>
2025-06-30 11:57:16 +0100

    fix norm errors on main.c

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ggomes-v <ggomes-v@student.42.fr>          +#+  +:+       +#+        */
+/*   By: joaomart <joaomart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 14:20:09 by ggomes-v          #+#    #+#             */
-/*   Updated: 2025/06/23 10:58:33 by ggomes-v         ###   ########.fr       */
+/*   Updated: 2025/06/30 11:55:20 by joaomart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,32 @@
 
 int	g_exit_status = 0;
 
+static void	handle_exit(t_shell *shell, char *line)
+{
+	if (line == NULL)
+	{
+		printf("exit\n");
+		free_struct(shell);
+		exit(shell->last_exit_status);
+	}
+}
+
+static void	process_line(char *line, t_shell *shell, t_token *token)
+{
+	if (*line)
+		add_history(line);
+	if (line != NULL && *line)
+		main_auxiliar(line, shell, token);
+}
+
 int	main(int ac, char **av, char **envp)
 {
-	(void) ac;
-	(void) av;
 	t_shell	*shell;
 	char	*line;
 	t_token	*token;
 
+	(void)ac;
+	(void)av;
 	token = NULL;
 	line = NULL;
 	shell = init_shell(ac, av, envp);
@@ -31,121 +49,48 @@ int	main(int ac, char **av, char **envp)
 	{
 		g_exit_status = shell->last_exit_status;
 		line = readline("minishell$ ");
-		if (line == NULL) // Ctrl-D
-		{
-			printf("exit\n");
-			free_struct(shell); // se tiveres cleanup
-			exit(shell->last_exit_status);
-		}
-		if (*line)
-			add_history(line);
-		if (line != NULL && *line)
-		{
-			main_auxiliar(line, shell, token);
-		}
+		handle_exit(shell, line);
+		process_line(line, shell, token);
 		free(line);
 	}
 	return (shell->last_exit_status);
 }
 
-int     main_auxiliar(char *line, t_shell *shell, t_token *token)
+int	main_auxiliar(char *line, t_shell *shell, t_token *token)
 {
-    t_exec_node *tree;
+	t_exec_node	*tree;
 
-    tree = NULL;
-    if (check_syntax_errors_main(line, shell) == 0)
-        token = tokenizer(line, shell);
-    if (token)
-    {
-        expander(&token, shell);
-        delete_quotes(&token, shell);
-        handle_heredoc(token, shell);
-        tree = build_execution_tree(token, NULL, shell);
-		//print_execution_tree(tree, 0);
-        execute_tree(tree, shell);
-        //execute_command(token, shell);
-        //printf("==========================================================\n");
-        //print_tokens(token, shell);
-    }
-    free_tokens(&token);
-    free_execution_tree(tree);
-    token = NULL;
-    return (0);
-}
-
-void print_tokens(t_token *list, t_shell *shell)
-{
-    if (!list)
-        return;
-    int i;
-    i = 0;
-    t_token *current = list;
-    while (current)
-    {
-        printf("token[%d] (%d): %s\n", i, current->type, current->value);
-        current = current->next;
-        i++;
-    }
-    printf("last_exit_status:%d\n", shell->last_exit_status);
-}
-
-/*void	print_node_type(t_node_type type)
-{
-	if (type == NODE_COMMAND)
-		printf("Type: COMMAND\n");
-	else if (type == NODE_PIPE)
-		printf("Type: PIPE\n");
-	else if (type == NODE_REDIRECT_IN)
-		printf("Type: REDIRECT_IN\n");
-	else if (type == NODE_REDIRECT_OUT)
-		printf("Type: REDIRECT_OUT\n");
-	else if (type == NODE_REDIRECT_APPEND)
-		printf("Type: REDIRECT_APPEND\n");
-	else
-		printf("Type: UNKNOWN\n");
-}
-
-void	print_command(char **cmd)
-{
-	int i = 0;
-
-	if (!cmd)
+	tree = NULL;
+	if (check_syntax_errors_main(line, shell) == 0)
+		token = tokenizer(line, shell);
+	if (token)
 	{
-		printf("Cmd: (null)\n");
-		return;
+		expander(&token, shell);
+		delete_quotes(&token, shell);
+		handle_heredoc(token, shell);
+		tree = build_execution_tree(token, NULL, shell);
+		execute_tree(tree, shell);
 	}
-	printf("Cmd:");
-	while (cmd[i])
+	free_tokens(&token);
+	free_execution_tree(tree);
+	token = NULL;
+	return (0);
+}
+
+void	print_tokens(t_token *list, t_shell *shell)
+{
+	int		i;
+	t_token	*current;
+
+	if (!list)
+		return ;
+	i = 0;
+	current = list;
+	while (current)
 	{
-		printf(" %s", cmd[i]);
+		printf("token[%d] (%d): %s\n", i, current->type, current->value);
+		current = current->next;
 		i++;
 	}
-	printf("\n");
+	printf("last_exit_status:%d\n", shell->last_exit_status);
 }
-
-void	print_execution_tree(t_exec_node *node, int depth)
-{
-	if (!node)
-		return;
-
-	// Indentação para visualizar a profundidade na árvore
-	for (int i = 0; i < depth; i++)
-		printf("  ");
-
-	print_node_type(node->type);
-
-	for (int i = 0; i < depth; i++)
-		printf("  ");
-	print_command(node->cmd);
-
-	for (int i = 0; i < depth; i++)
-		printf("  ");
-	printf("FD_IN: %d | FD_OUT: %d\n", node->fd_in, node->fd_out);
-
-	printf("\n");
-
-	// Recursivamente imprime os nós à esquerda e à direita
-	print_execution_tree(node->left, depth + 1);
-	print_execution_tree(node->right, depth + 1);
-}*/
-

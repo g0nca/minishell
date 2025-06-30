1ae108bc2962f932507b5861bb1bf5c30037dac0
Gonçalo Gomes Valente <ggomes-v@c2r3s2.42porto.com>
2025-06-30 11:35:47 +0100

    execution tree norm

--- srcs/binary_tree/execution_tree_token_utils.c
+++ srcs/binary_tree/execution_tree_token_utils.c
@@ -6,7 +6,7 @@
 /*   By: ggomes-v <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 11:12:40 by ggomes-v          #+#    #+#             */
-/*   Updated: 2025/05/29 11:16:33 by ggomes-v         ###   ########.fr       */
+/*   Updated: 2025/06/30 11:34:17 by ggomes-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,10 @@ int	count_valid_tokens(t_token *start, t_token *end)
 		}
 		if (is_redirection(tmp->type))
 		{
-			skip_next = 1;  // Skip the next token (filename)
+			skip_next = 1;
 		}
 		else if (tmp->type == TOKEN_WORD || tmp->type == TOKEN_CMD)
-		{
 			count++;
-		}
 		tmp = tmp->next;
 	}
 	return (count);
@@ -55,7 +53,18 @@ void	cleanup_argv_on_error(char **argv, int count)
 	free(argv);
 }
 
-void	fill_argv_array(t_token *start, t_token *end, char **argv, int count)
+int	add_token_to_argv(char **argv, int i, char *value)
+{
+	argv[i] = ft_strdup(value);
+	if (!argv[i])
+	{
+		cleanup_argv_on_error(argv, i);
+		return (0);
+	}
+	return (1);
+}
+
+void	fill_argv_array(t_token *start, t_token *end, char **argv, int *count)
 {
 	t_token	*tmp;
 	int		i;
@@ -64,26 +73,19 @@ void	fill_argv_array(t_token *start, t_token *end, char **argv, int count)
 	i = 0;
 	tmp = start;
 	skip_next = 0;
-	while (tmp && tmp != end && i < count)
+	while (tmp && tmp != end && i < *count)
 	{
 		if (skip_next)
 		{
 			skip_next = 0;
 			tmp = tmp->next;
-			continue ;
 		}
 		if (is_redirection(tmp->type))
-		{
-			skip_next = 1;  // Skip the next token (filename)
-		}
+			skip_next = 1;
 		else if (tmp->type == TOKEN_WORD || tmp->type == TOKEN_CMD)
 		{
-			argv[i] = ft_strdup(tmp->value);
-			if (!argv[i])
-			{
-				cleanup_argv_on_error(argv, i);
+			if (!add_token_to_argv(argv, i, tmp->value))
 				return ;
-			}
 			i++;
 		}
 		tmp = tmp->next;

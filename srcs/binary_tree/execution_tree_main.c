9b2f9c2ae82f4ecf296b159ea7713bb9679ca78d
andrade950 <joao2005andrade@gmail.com>
2025-07-01 15:53:50 +0100

    Quit (core dumped) problem solve

--- srcs/binary_tree/execution_tree_main.c
+++ srcs/binary_tree/execution_tree_main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execution_tree_main.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joaomart <joaomart@student.42.fr>          +#+  +:+       +#+        */
+/*   By: andrade <andrade@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 11:01:13 by ggomes-v          #+#    #+#             */
-/*   Updated: 2025/06/30 11:01:39 by joaomart         ###   ########.fr       */
+/*   Updated: 2025/07/01 15:52:39 by andrade          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ void	execute_command_tree(t_exec_node *node, t_shell *shell)
 {
 	pid_t	pid;
 	int		status;
+	void	(*prev_sigint)(int);
 
 	if (is_builtin(node->cmd[0]))
 		execute_command_node(node, shell);
@@ -47,12 +48,27 @@ void	execute_command_tree(t_exec_node *node, t_shell *shell)
 		pid = fork();
 		if (pid == 0)
 		{
+			signal(SIGINT, SIG_DFL);
+			signal(SIGQUIT, SIG_DFL);
+			if (node->fd_in == -1)
+				dup2(STDIN_FILENO, 0);
+			if (node->fd_out == -1)
+				dup2(STDOUT_FILENO, 1);
 			execute_command_node(node, shell);
 			exit(shell->last_exit_status);
 		}
 		else if (pid > 0)
 		{
+			prev_sigint = signal(SIGINT, SIG_IGN);
 			waitpid(pid, &status, 0);
+			signal(SIGINT, prev_sigint);
+			if (WIFSIGNALED(status))
+			{
+				if (WTERMSIG(status) == SIGINT)
+					write(STDOUT_FILENO, "\n", 1);
+				else if (WTERMSIG(status) == SIGQUIT)
+					write(STDOUT_FILENO, "Quit (core dumped)\n", 19);
+			}
 			shell->last_exit_status = WEXITSTATUS(status);
 		}
 	}

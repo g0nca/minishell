9fbe12a21ef282883b5afe1fb01a3ebce8f87cc7
andrade950 <joao2005andrade@gmail.com>
2025-07-01 15:09:16 +0100

    fix mutiple redirects error

--- srcs/binary_tree/execution_tree_build_main.c
+++ srcs/binary_tree/execution_tree_build_main.c
@@ -12,20 +12,61 @@
 
 #include "../../inc/minishell.h"
 
+static t_exec_node	*wrap_with_redirects(t_token *start, t_token *end, t_shell *shell)
+{
+    t_token		*curr;
+    t_exec_node	*cmd_node;
+    t_exec_node	*redir_node;
+	(void)shell; // shell is not used in this function, but kept for consistency
+
+    cmd_node = create_command_node(start, end);
+    if (!cmd_node)
+        return (NULL);
+
+    curr = start;
+    while (curr && curr != end)
+    {
+        if (is_redirection(curr->type) && curr->next && curr->next->type == TOKEN_WORD)
+        {
+            redir_node = malloc(sizeof(t_exec_node));
+            if (!redir_node)
+            {
+                free_execution_tree(cmd_node);
+                return (NULL);
+            }
+            redir_node->type = get_redirect_node_type(curr->type);
+            redir_node->cmd = malloc(sizeof(char *) * 2);
+            if (!redir_node->cmd)
+            {
+                free(redir_node);
+                free_execution_tree(cmd_node);
+                return (NULL);
+            }
+            redir_node->cmd[0] = ft_strdup(curr->next->value);
+            redir_node->cmd[1] = NULL;
+            redir_node->fd_in = -1;
+            redir_node->fd_out = -1;
+            redir_node->left = cmd_node;
+            redir_node->right = NULL;
+            cmd_node = redir_node;
+            curr = curr->next; // skip filename
+        }
+        curr = curr->next;
+    }
+    return cmd_node;
+}
+
 t_exec_node	*build_execution_tree(t_token *start, t_token *end, t_shell *shell)
 {
-	t_exec_node	*redirect_node;
-	t_token		*last_pipe;
+    t_token		*last_pipe;
 
-	if (!start || start == end)
-		return (NULL);
-	last_pipe = find_last_pipe(start, end);
-	if (last_pipe)
-		return (create_pipe_node(start, last_pipe, end, shell));
-	redirect_node = find_and_create_redirect_node(start, end, shell);
-	if (redirect_node)
-		return (redirect_node);
-	return (create_command_node(start, end));
+    if (!start || start == end)
+        return (NULL);
+    last_pipe = find_last_pipe(start, end);
+    if (last_pipe)
+        return (create_pipe_node(start, last_pipe, end, shell));
+    // Instead of find_and_create_redirect_node, use the new wrapper
+    return (wrap_with_redirects(start, end, shell));
 }
 
 void	free_execution_tree(t_exec_node *node)

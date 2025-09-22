01bf12b75252f300130c370a3ca47fe022ab24e0
Gonca <ggomes-v@c2r2s3.42porto.com>
2025-09-22 09:58:33 +0100

    This branch 22_09

--- srcs/binary_tree/execution_tree_build_main2.c
+++ srcs/binary_tree/execution_tree_build_main2.c
@@ -12,52 +12,46 @@
 
 #include "../../inc/minishell.h"
 
-static int	open_input_file(t_exec_node *cmd, char *filename)
+static int	open_input_file(t_exec_node *cmd, char *filename, t_shell *shell)
 {
 	cmd->fd_in = open(filename, O_RDONLY);
-	if (cmd->fd_in < 0)
+	if (cmd->fd_in == -1)
 	{
-		perror("minishell");
+		shell_error(shell, filename, 2, EXIT_SUCCESS);
 		return (-1);
 	}
 	return (0);
 }
 
-static int	open_output_file(t_exec_node *cmd, char *filename, int append)
+static int  open_output_file(t_exec_node *cmd, char *filename,
+        int append, t_shell *shell)
 {
-	int	flags;
+    static int  error_reported;
 
-	if (append)
-		flags = O_WRONLY | O_CREAT | O_APPEND;
-	else
-		flags = O_WRONLY | O_CREAT | O_TRUNC;
-	cmd->fd_out = open(filename, flags, 0644);
-	if (cmd->fd_out < 0)
-	{
-		perror("minishell");
-		return (-1);
-	}
-	return (0);
+	error_reported = 0;
+	if (append != 1)
+        cmd->fd_out = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    else
+        cmd->fd_out = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (cmd->fd_out == -1)
+    {
+        if (!error_reported)
+        {
+            shell_error(shell, filename, 3, EXIT_SUCCESS);
+            error_reported = 1;
+        }
+        return (-1);
+    }
+    return (0);
 }
 
-static int	apply_redirects(t_exec_node *cmd, char *in, char *out, char *append)
-{
-	if (in && open_input_file(cmd, in) < 0)
-		return (-1);
-	if (out && open_output_file(cmd, out, 0) < 0)
-		return (-1);
-	if (append && open_output_file(cmd, append, 1) < 0)
-		return (-1);
-	return (0);
-}
-
-static int	process_redirects(t_token *start,
-	t_token *end, t_shell *shell, t_redirs *redirs)
+static int	process_redirects(t_token *start, 
+		t_exec_node	*cmd, t_shell *shell, t_redirs *redirs)
 {
 	t_token	*curr;
 
 	curr = start;
-	while (curr && curr != end)
+	while (curr && curr != NULL)
 	{
 		if (curr->type == TOKEN_HERE_DOC
 			&& curr->next && curr->next->type == TOKEN_WORD)
@@ -68,13 +62,28 @@ static int	process_redirects(t_token *start,
 		}
 		else if (curr->type == TOKEN_REDIR_IN
 			&& curr->next && curr->next->type == TOKEN_WORD)
+		{
 			redirs->in = curr->next->value;
+			curr = curr->next;
+			if (open_input_file(cmd, curr->value, shell) < 0)
+				return (-1);
+		}
 		else if (curr->type == TOKEN_REDIR_OUT
 			&& curr->next && curr->next->type == TOKEN_WORD)
+		{
 			redirs->out = curr->next->value;
+			curr = curr->next;
+			if (open_output_file(cmd, curr->value, false, shell) < 0)
+				return (-1);
+		}
 		else if (curr->type == TOKEN_APPEND
 			&& curr->next && curr->next->type == TOKEN_WORD)
+		{
 			redirs->append = curr->next->value;
+			curr = curr->next;
+			if (open_output_file(cmd, curr->value, true, shell) < 0)
+				return (-1);
+		}
 		curr = curr->next;
 	}
 	return (0);
@@ -89,15 +98,15 @@ t_exec_node	*wrap_with_redirects(t_token *start, t_token *end, t_shell *shell)
 	cmd = create_command_node(start, end);
 	if (!cmd)
 		return (NULL);
-	if (process_redirects(start, end, shell, &redirs) < 0)
+	if (process_redirects(start, cmd, shell, &redirs) < 0)
 	{
 		shell->last_exit_status = 1;
 		return (free_execution_tree(cmd), NULL);
 	}
-	if (apply_redirects(cmd, redirs.in, redirs.out, redirs.append) < 0)
+	/*if (apply_redirects(cmd, &redirs) < 0)
 	{
 		shell->last_exit_status = 1;
 		return (free_execution_tree(cmd), NULL);
-	}
+	}*/
 	return (cmd);
 }

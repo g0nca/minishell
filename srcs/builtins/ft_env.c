5c1aab2462f2ca6e5bfb0fe18bcd4d91e2c021f7
Gonca <ggomes-v@c2r2s3.42porto.com>
2025-10-09 14:26:15 +0100

    ft_env updated, the empty enviroment variables do not appear on env command, only on export

--- srcs/builtins/ft_env.c
+++ srcs/builtins/ft_env.c
@@ -6,11 +6,12 @@
 /*   By: ggomes-v <ggomes-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 17:08:39 by joaomart          #+#    #+#             */
-/*   Updated: 2025/10/09 12:41:16 by ggomes-v         ###   ########.fr       */
+/*   Updated: 2025/10/09 14:21:41 by ggomes-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
 
 void	ft_env(t_shell *shell)
 {
@@ -19,7 +20,8 @@ void	ft_env(t_shell *shell)
 	i = 0;
 	while (shell->env[i])
 	{
-		printf("%s\n", shell->env[i]);
+		if (ft_strchr(shell->env[i], '='))
+			printf("%s\n", shell->env[i]);
 		i++;
 	}
 	shell->last_exit_status = EXIT_SUCCESS;
